41e381f5ecc91b5c9e4a20391ab75b80c862b28f
metal <ralmeidaob@alumnos.unex.es>
2025-11-08 17:31:05 +0100

    initialize Gumia project with Spring Boot, JPA, and MySQL configuration; add entity models and repositories

--- src/main/java/com/Gumia/repositories/RecetaRepository.java
+++ src/main/java/com/Gumia/repositories/RecetaRepository.java
@@ -3,8 +3,4 @@ package com.Gumia.repositories;
 import com.Gumia.model.Receta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RecetaRepository extends JpaRepository<Receta, Long> {
-    public Receta findByNombre(String nombre);
-    public Receta sortByDificultadAsc();
-    public Receta sortByDificultadDesc();
-}
+public interface RecetaRepository extends JpaRepository<Receta, Long> { }
